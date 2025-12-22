e8bbd97d42c93580bbc85758c4b615394294bd16
Raghav <raghavmahajanofficial09@gmail.com>
2025-12-22 23:46:27 +0530

    Time: 2 ms (23.28%), Space: 12.7 MB (86.77%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n=strs.size(), m=strs[0].size();
-        bitset<100> isSorted=0, remove=0;
-        for (int j=0; j<m; j++) {
-            for (int i=0; i<n-1; i++) {
-                if (isSorted[i]==0 & strs[i][j]>strs[i+1][j]) {
-                    remove[j]=1;
-                    break;
+        int n = strs.size() , m = strs[0].size() ; 
+        // int ans = 0 ; 
+        set<int> deletion ; 
+        for(int j=0 ; j<m ; j++){
+            char prev = 'a' ;
+            for(int i=0 ; i<n ; i++){
+                if(strs[i][j]>=prev){
+                    prev = strs[i][j] ; 
+                }
+                else{ // Collision
+                    bool check = true ; 
+                    for(int k = j-1 ; k>=0 ; k--){
+                        if(deletion.find(k)!=deletion.end()) continue ; 
+                        if(strs[i][k] > strs[i-1][k]){
+                            check = false ; 
+                            break ; 
+                        }
+                    }
+                    if(check){
+                        deletion.insert(j) ; 
+                        break ; 
+                    }
+                    prev = strs[i][j] ; 
                 }
             }
-
-            if (remove[j]) continue;
-            
-            for (int i=0; i<n-1; i++) 
-                isSorted[i]=isSorted[i]|(strs[i][j]<strs[i+1][j]);
         }
-        return remove.count();
+        return deletion.size() ; 
     }
 };
+
+/*
+
+
+
+
+
+*/
