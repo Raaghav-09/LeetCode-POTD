ae59cf50c6630f7910caea1db20718ab3b209beb
Raghav <raghavmahajanofficial09@gmail.com>
2026-02-12 18:54:46 +0530

    Time: 1661 ms (5.03%), Space: 381.7 MB (5.43%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isSafe(int x , int y , int n , int m){
+        return x>=0 && y>=0 && x<n && y<m ; 
+    }
+    bool f(int x , int y , vector<vector<char>>& board , vector<vector<bool>>& visited , string& word , int ind){
+        int n = board.size() , m = board[0].size() ; 
+        int sz = word.size() ; 
+
+        if(ind == sz) return true ; 
+
+        vector<pair<int,int>> dirs = {{-1,0} , {0,-1} , {0,1} , {1,0}} ; 
+        
+        bool ans = false ; 
+        for(auto [dx,dy] : dirs){
+            int nx = x+dx , ny = y + dy ; 
+            if(isSafe(nx,ny,n,m) && !visited[nx][ny] && board[nx][ny] == word[ind]){
+                visited[nx][ny] = true ; 
+                ans |= f(nx , ny , board , visited , word , ind+1) ; 
+                if(ans) return true ; 
+                visited[nx][ny] = false ; 
+            }
+        }
+
+        return ans ; 
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size() , m = board[0].size() ; 
+        vector<pair<int,int>> v ; 
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(board[i][j] == word[0]){
+                    v.push_back(make_pair(i,j)) ; 
+                }
+            }
+        }
+        for(auto [x,y] : v){
+            vector<vector<bool>> visited(n,vector<bool>(m,false)) ; 
+            visited[x][y] = true ; 
+            if(f(x,y,board , visited , word , 1)) return true ;
+        }
+        return false ; 
+    }
+};
