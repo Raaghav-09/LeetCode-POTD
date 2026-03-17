857f17673c9e54a90fc4bd8ec1572df7e900d0f3
Raghav <raghavmahajanofficial09@gmail.com>
2026-03-17 10:42:49 +0530

    Time: 416 ms (5.02%), Space: 216.5 MB (5%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size() ; 
-        vector<int> ans ;
-        map<int , int > mp ; 
-        int i=0 , j = 0 ; 
-        while(j<k){
-            mp[nums[j]]++ ; 
-            j++ ; 
+        vector<int> ans ; 
+        multiset<int> mul ; 
+        for(int i=0 ; i<k ; i++){
+            mul.insert(nums[i]) ; 
         }
-        ans.push_back(mp.rbegin()->first) ; 
-        while(j<n){
-            mp[nums[j]]++ ; 
-            mp[nums[i]]-- ; 
-            if(mp[nums[i]] == 0) mp.erase(nums[i]) ; 
-            i++ ; 
-            j++ ; 
-            ans.push_back(mp.rbegin()->first) ; 
+        ans.push_back(*mul.rbegin()) ; 
+        for(int i=k ; i<n ; i++){
+            mul.insert(nums[i]) ; 
+            mul.erase(mul.find(nums[i-k])) ; 
+            ans.push_back(*mul.rbegin()) ; 
         }
+
         return ans ; 
     }
 };
