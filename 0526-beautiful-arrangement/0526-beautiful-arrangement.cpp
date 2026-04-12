e8b64716ebf8437a215963b61d345af0a114bfaa
Raghav <raghavmahajanofficial09@gmail.com>
2026-04-12 20:28:28 +0530

    Time: 7 ms (84.54%), Space: 18.7 MB (13.06%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int countArrangement(int n) {
+        vector<vector<int>> dp(n+1,vector<int>(1<<(n+1) , -1)) ; 
         auto f = [&](int ind , int mask , auto&& f) -> int{
             if(ind == n){
                 return 1 ; 
             }
+            if(dp[ind][mask] != -1) return dp[ind][mask] ; 
             int ways = 0 ; 
             int pos = ind+1 ; 
             for(int j=1 ; j<=n ; j++){
@@ -13,7 +15,7 @@ public:
                 }
             }
 
-            return ways ; 
+            return dp[ind][mask] = ways ; 
         };
 
         return f(0,0,f) ; 
