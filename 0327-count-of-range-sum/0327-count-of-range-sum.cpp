531e9754f81072f178be0eb084302617d560014a
Raghav <raghavmahajanofficial09@gmail.com>
2026-06-07 12:48:32 +0530

    Time: 347 ms (55.79%), Space: 211.4 MB (44.36%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.cpp
+++ 0327-count-of-range-sum/0327-count-of-range-sum.cpp
@@ -1,33 +1,70 @@
 class Solution {
 public:
-    using ll = long long;
-
-    int countRangeSum(vector<int>& nums, int lower, int upper){
-        int n = nums.size();
+    using ll = long long ; 
+    int countRangeSum(vector<int>& nums , int lower, int upper){
+        int n = nums.size() ; 
 
         vector<ll> pre(n + 1, 0);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++){
             pre[i + 1] = pre[i] + nums[i];
         }
 
-        multiset<ll> mul;
-        mul.insert(0);  // prefix before first element
+        auto merge = [&](int l , int mid , int r) -> int{
+            int count = 0;
+            int start = mid + 1, end = mid + 1;
+            
+            for(int i = l ; i <= mid ; i++){
+                while(start <= r && pre[start] - pre[i] < lower){
+                    start++;
+                }
+                while(end <= r && pre[end] - pre[i] <= upper){
+                    end++;
+                }
+                count += (end - start);
+            }
+            vector<ll> v(r - l + 1) ; 
+            int k = 0 ; 
+            int i = l , j = mid + 1; 
+            while(i <= mid && j <= r){
+                if(pre[i] < pre[j]){
+                    v[k] = pre[i] ; 
+                    i++ , k++ ; 
+                }
+                else{
+                    v[k] = pre[j] ; 
+                    j++ , k++ ; 
+                }
+            }
 
-        int ans = 0;
+            while(i <= mid){
+                v[k] = pre[i] ; 
+                k++ , i++ ; 
+            }
 
-        for (int i = 1; i <= n; i++) {
+            while(j <= r){
+                v[k] = pre[j] ; 
+                k++ , j++ ; 
+            }
 
-            ll low = pre[i] - upper;
-            ll high = pre[i] - lower;
+            for(int idx = l ; idx <= r ; idx++){
+                pre[idx] = v[idx - l] ; 
+            }
+            return count ; 
+        };
 
-            auto l = mul.lower_bound(low);
-            auto r = mul.upper_bound(high);
+        auto f = [&](auto&& self , int l , int r) -> int {
+            if(l >= r){
+                 return 0 ; 
+            }
+            int mid = l + (r - l) / 2 ; 
 
-            ans += distance(l, r);
+            int val1 = self(self, l, mid) ; 
+            int val2 = self(self, mid + 1, r) ; 
 
-            mul.insert(pre[i]);
-        }
+            int val3 = merge(l, mid, r) ;
+            return val1 + val2 + val3 ; 
+        };
 
-        return ans;
+        return f(f, 0, n) ; 
     }
 };
