e58b60977b9935aaf076bdc2cc91dd281e2e5277
Raghav <raghavmahajanofficial09@gmail.com>
2026-06-09 15:01:11 +0530

    Time: 1849 ms (5.04%), Space: 32.3 MB (16.95%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int minCut(string s) {
-        int n = s.size() ; 
-        vector<vector<int>> isPal(n,vector<int>(n,-1)) ; 
-        auto isPalindrome = [&](int ind1 , int ind2 , auto&& self) -> int{
-            if(isPal[ind1][ind2] != -1) return isPal[ind1][ind2] ; 
-            if(ind1 > ind2) return isPal[ind1][ind2] = true  ; 
-            if(ind1 == ind2) return isPal[ind1][ind2] = true ; 
-            if(ind1 == ind2-1 && s[ind1] == s[ind2]) return isPal[ind1][ind2] = true ; 
-            if(s[ind1] == s[ind2]){
-                return isPal[ind1][ind2] = self(ind1+1,ind2-1,self) ; 
+    bool isPalindrome(string& s){
+        int i = 0 , j = s.size() -1 ; 
+        while(i<=j){
+            if(s[i] == s[j]){
+                i++ , j-- ; 
             }
-            else return isPal[ind1][ind2] = false ; 
-        };
-
-        for(int i=0 ; i<n ; i++){
-            for(int j=i ; j<n ; j++){
-                isPalindrome(i,j,isPalindrome) ; 
-            }
+            else return false ;
         }
-        vector<int> dp(n,-1) ; 
-        auto f = [&](int ind,auto&& self) -> int{
-            if(ind == n) return 0 ; 
-            if(dp[ind] != -1) return dp[ind] ; 
-            int answer = INT_MAX ; 
-            for(int i = ind ; i<n ; i++){
-                if(isPal[ind][i]){
-                    int res = self(i+1,self) ;
-                    answer = min(answer , 1 + res) ; 
+        return true ; 
+    }
+    int dp[2005] ; 
+    int minCut(string s) {
+        int n = s.size() ;  
+
+        // dp[i][j] = minimum cuts to make i to j palindromic
+        // dp[i][j] = 1 + min( dp[i][k] + dp[k+1][j] )
+
+        auto f = [&](auto&& f , int idx ) -> int{
+            if(idx == n) return 0 ; 
+            if(dp[idx] != -1) return dp[idx] ; 
+            string temp = "" ; 
+            int res = n ; 
+            for(int p = idx ; p<n ; p++){
+                temp += s[p] ; 
+                if(isPalindrome(temp)){
+                    res = min(res , 1 + f(f,p+1)) ; 
                 }
             }
-            return dp[ind] = answer ; 
-        };
 
-        return f(0,f)-1 ; 
+            return dp[idx] = res ; 
+        };
+        memset(dp,-1,sizeof(dp)) ; 
+        return f(f,0) - 1 ; 
     }
 };
