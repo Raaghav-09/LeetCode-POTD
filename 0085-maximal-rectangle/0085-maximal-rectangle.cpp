a1a6dad25a37b35214e08aabe01c464734b2d5ea
Raghav <raghavmahajanofficial09@gmail.com>
2026-06-09 18:11:00 +0530

    Time: 22 ms (13.22%), Space: 23.3 MB (12.7%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,59 +1,73 @@
 class Solution {
-public:
-    int maxArea(vector<int>& v){
-        int n = v.size();
-        vector<int> prevSmallerIdx(n), nextSmallerIdx(n);
-        stack<int> st;
+private:
+    int mxAreaOfHistogram(vector<int>& h){
+        int n = h.size() ; 
 
-        // Previous smaller element
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && v[st.top()] >= v[i])
-                st.pop();
-            prevSmallerIdx[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
+        /*
+        3 1 3 2 2
+        => largest area of rectangle in a histogram
 
-        while(!st.empty()) st.pop();
+        how much i can extend from any index to left and to right 
+        if i get that than i can find the largest area for that h[i]
 
-        // Next smaller element
-        for(int i = n - 1; i >= 0; i--){
-            while(!st.empty() && v[st.top()] >= v[i])
-                st.pop();
-            nextSmallerIdx[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
+        */
 
-        int area = 0;
-        for(int i = 0; i < n; i++){
-            int width = nextSmallerIdx[i] - prevSmallerIdx[i] - 1;
-            area = max(area, v[i] * width);
-        }
-        return area;
-    }
-
-    vector<int> f(int row, vector<vector<char>>& grid){
-        int m = grid[0].size();
-        vector<int> v(m, 0);
-
-        for(int j = 0; j < m; j++){
-            int cnt = 0;
-            for(int i = row; i >= 0; i--){
-                if(grid[i][j] == '1') cnt++;
-                else break;
+        stack<int> st ; 
+        vector<int> prevSmaller(n,0) ;
+        prevSmaller[0] = -1 ; 
+        st.push(-1) ;
+        for(int i=0 ; i<n ; i++){
+            while(st.top()!=-1 && h[st.top()] >= h[i]){
+                st.pop() ; 
             }
-            v[j] = cnt;
+            prevSmaller[i] = st.top() ; 
+            st.push(i) ; 
         }
-        return v;
+
+        while(st.size()) st.pop() ; 
+        vector<int> nextSmaller(n,0) ;
+        st.push(n) ; 
+        for(int i=n-1 ; i>=0 ; i--){
+            while(st.top()!=n && h[st.top()] >= h[i]){
+                st.pop() ; 
+            }
+            nextSmaller[i] = st.top() ; 
+            st.push(i) ; 
+        }
+
+        int ans = 0 ; 
+
+        for(int i=0 ; i<n ; i++){
+            int val = h[i]*(nextSmaller[i] - prevSmaller[i] - 1) ; 
+            ans = max(ans,val) ; 
+        }
+        return ans ; 
     }
-
+public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int ans = 0;
+        int n = matrix.size() , m = matrix[0].size() ; 
 
-        for(int i = 0; i < n; i++){
-            vector<int> v = f(i, matrix);
-            ans = max(ans, maxArea(v));
+        vector<vector<int>> pre(n,vector<int>(m,0)) ; 
+        for(int i=0 ; i<m ; i++) pre[0][i] = matrix[0][i] - '0' ; 
+
+        for(int i=1 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(matrix[i][j] == '0') pre[i][j] = 0 ; 
+                else pre[i][j] = pre[i-1][j] + 1 ; 
+            }
         }
-        return ans;
+
+        /*
+        1 0 1 0 0 
+        2 0 2 1 1
+        3 1 3 2 2
+        4 0 0 3 0
+        */
+        int ans = 0 ; 
+        for(int i=0 ; i<n ; i++){
+            ans = max(ans,mxAreaOfHistogram(pre[i])) ; 
+        }
+
+        return ans ; 
     }
 };
