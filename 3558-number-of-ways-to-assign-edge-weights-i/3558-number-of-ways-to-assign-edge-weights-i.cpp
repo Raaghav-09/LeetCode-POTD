24e48d63988613befa7b865a68eb05172fe8aabb
Raghav <raghavmahajanofficial09@gmail.com>
2026-06-12 13:02:01 +0530

    Time: 563 ms (15.13%), Space: 452.1 MB (5.26%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
-    const int MOD=1e9+7;
-int power(long long a,long long b){
-    long long ans=1;
-        a%=MOD;
-        while(b>0){
-            if(b&1)ans=(ans*a)%MOD;
-            a=(a*a)%MOD;
-            b>>=1;
-        }
-    return (int)ans;
-}
-    int dfs(int node,int per,vector<vector<int>>&adj){
-        if(adj[node].size()==0)return 0;
-        int ans=0;
-        for(auto&it:adj[node]){
-            if(it!=per)ans=max(ans,1+dfs(it,node,adj));
-        }
-        return ans;
-    }
+    using ll = long long ;
+    const ll mod = 1e9 + 7 ; 
+
     int assignEdgeWeights(vector<vector<int>>& edges) {
-        int n=edges.size()+2;
-        vector<vector<int>>adj(n);
-        for(auto&edge : edges){
-            int u=edge[0],v=edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        int n = edges.size() ; 
+        n++ ; 
+        /*
+        suppose distance between u and v = odd
+        Then , 
+        odd => no of one should be odd 
+        even => no 
+
+        It means in u to v we should have odd no of ones . 
+
+        It just means we need to find distance between u to v => d 
+        and ans = dC1 + dC3 + dC5 + ... dCd = 2^(d-1)
+        ans *= pow(2,(n-1-d))
+        */
+
+        vector<ll> powerOf2(n,0) ; 
+        powerOf2[0] = 1 ; 
+        for(int i=1 ; i<n ; i++){
+            powerOf2[i] = (powerOf2[i-1]*2) % mod ; 
         }
-        int depth=dfs(1,-1,adj);
-        return power(2,depth-1);
+
+        vector<vector<int>> adj(n) ; 
+        for(int i=0 ; i<n-1 ; i++){
+            adj[edges[i][0]-1].push_back(edges[i][1]-1) ; 
+            adj[edges[i][1]-1].push_back(edges[i][0]-1) ; 
+        }
+
+
+        // binary lifting table 
+        int sz = log2(n) + 2 ; 
+        vector<int> depth(n,0); 
+        depth[0] = 0 ; 
+        int deepest = 0 ; 
+        vector<vector<int>> par(n,vector<int>(sz,-1)) ; 
+        auto dfs = [&](auto&& dfs , int node , int parent) -> void{
+            if(parent != -1) depth[node] = depth[parent] + 1 ; 
+            if(depth[node] > deepest) deepest = depth[node];
+            par[node][0] = parent ; 
+            for(int i=1 ; i<sz ; i++){
+                if(par[node][i-1] != -1) par[node][i] = par[par[node][i-1]][i-1] ; 
+                else par[node][i] = -1 ; 
+            }
+            for(auto adjNode : adj[node]){
+                if(adjNode == parent) continue ; 
+                dfs(dfs,adjNode,node) ; 
+            }
+        };
+
+        dfs(dfs,0,-1) ; 
+
+        int res = powerOf2[deepest-1] ; 
+        return res ; 
     }
 };
