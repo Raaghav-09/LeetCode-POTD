6fd532e82d64aabea9534d1ee2376c5a47571d93
Raghav <raghavmahajanofficial09@gmail.com>
2026-06-22 10:14:31 +0530

    Time: 205 ms (33.61%), Space: 153.7 MB (15.01%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        // for half of the element should have their half to be present inside the array
         int n = changed.size() ; 
         if(n%2 == 1) return {} ; 
-        map<int,int> mp ; 
+        sort(changed.begin(),changed.end()) ; 
+
+        vector<int> original ; 
+        map<int,int> skip ;
         for(int i=0 ; i<n ; i++){
-            mp[changed[i]]++ ; 
-        }
-
-        sort(changed.begin() , changed.end()) ; 
-
-        vector<int> ans ; 
-        if(mp.find(0) != mp.end()){
-            if(mp[0] % 2 == 1) return ans ; 
+            int val = 2*changed[i] ; 
+            if(skip.count(changed[i])){
+                skip[changed[i]]-- ; 
+                if(skip[changed[i]] == 0) skip.erase(changed[i]) ; 
+            }
             else{
-                int freq = mp[0]/2 ; 
-                while(freq--) ans.push_back(0) ; 
-                mp.erase(0) ;
+                original.push_back(changed[i]) ; 
+                skip[val]++ ; 
             }
         }
 
-        for(int i=0 ; i<n ; i++){
-            if(changed[i] == 0) continue ;
-            if(mp.find(changed[i]) == mp.end()) continue ;  
-            ans.push_back(changed[i]) ; 
-            mp[changed[i]]-- ; 
-            int toRemove = 2*changed[i] ; 
-            if(mp.find(toRemove) == mp.end()) return {} ; 
-            mp[toRemove]-- ; 
-            if(mp[changed[i]] == 0) mp.erase(changed[i]) ; 
-            if(mp[toRemove] == 0) mp.erase(toRemove) ;  
-            if(ans.size() == n/2) break ; 
-        }
-        if(ans.size() != n/2) return {} ; 
-        return ans; 
+        if(skip.size() != 0) return {};
+        return original ; 
     }
 };
