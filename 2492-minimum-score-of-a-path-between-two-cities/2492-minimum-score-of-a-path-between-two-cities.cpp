7b65a61b815a748f6fdde02c0917132f248afb3b
Raghav <raghavmahajanofficial09@gmail.com>
2026-07-04 10:35:55 +0530

    Time: 12 ms (94.9%), Space: 107.5 MB (95.25%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,71 @@
+const int inf = 1e9 ; 
+class UnionFind{
+private:
+    vector<int> parent ;
+    vector<int> size ; 
+public:
+    UnionFind(int n){
+        size.assign(n,1) ; 
+        parent.assign(n,0) ; 
+        iota(parent.begin(),parent.end(),0) ; 
+    }
+    int unionParent(int node){
+        if(parent[node] == node) return node ; 
+        return parent[node] = unionParent(parent[node]) ; 
+    }
+    void unite(int u , int v){
+        u = unionParent(u) , v = unionParent(v) ;
+        if(u != v){
+            if(size[u] > size[v]) swap(u,v) ; 
+            // size[u] <= size[v]
+            parent[u] = v ;
+            size[v] += size[u] ;   
+        }
+    }
+};
+
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        // vector<vector<pair<int,int>>> adj(n) ; 
+        // for(auto road : roads){
+        //     int u = road[0] , v = road[1] , w = road[2] ; 
+        //     u-- , v-- ; 
+        //     adj[u].push_back(make_pair(v,w)) ; 
+        //     adj[v].push_back(make_pair(u,w)) ; 
+        // }
+
+        // int mn = 1e9 ; 
+        // vector<int> visited(n,0) ; 
+        // auto dfs = [&](auto&& dfs , int node , int mnYet) -> void{
+        //     visited[node] = 1 ; 
+        //     if(node == n-1){
+
+        //     }
+        //     for(auto& [adjNode,weight] : adj[node]){
+        //         mnYet = min(weight,mnYet) ; 
+        //         if(!visited[adjNode]){
+        //             dfs(dfs,adjNode,mnYet) ; 
+        //         }
+        //     }
+        // };
+
+        UnionFind dsu(n) ; 
+        for(auto& road : roads){
+            int u = road[0] , v = road[1] ; 
+            u-- , v-- ; 
+            dsu.unite(u,v) ; 
+        }   
+        int ans = 1e9 ; 
+        int par = dsu.unionParent(0) ; 
+        for(auto& road : roads){
+            int u = road[0] , v = road[1] , w = road[2] ; 
+            u-- , v-- ; 
+            if(par == dsu.unionParent(u)){
+                ans = min(ans,w) ; 
+            }
+        }
+
+        return ans ; 
+    }
+};
