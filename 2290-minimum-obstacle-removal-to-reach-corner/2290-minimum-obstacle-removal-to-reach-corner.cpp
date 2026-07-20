61180b2aca4e06712355114adf5a54ff8895f9ad
Raghav <raghavmahajanofficial09@gmail.com>
2026-07-20 20:31:05 +0530

    Time: 711 ms (18%), Space: 276.9 MB (9.6%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -2,35 +2,36 @@ class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size() ; 
-        vector<vector<int>> dist(n,vector<int>(m,1e9)) ; 
-        deque<pair<int,int>> dq ; 
 
-        dq.push_front({0,0}) ; 
-        dist[0][0] = 0 ; 
+        vector<pair<int,int>> dirs = {{-1,0},{0,-1},{0,1},{1,0}} ; 
 
-        vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
+        deque<vector<int>> dq ; 
+        dq.push_front({0,0,0}) ; 
+        vector<vector<int>> removals(n,vector<int>(m,INT_MAX)) ; 
+        removals[0][0] = 0 ; 
         while(dq.size()){
-            int x = dq.front().first , y = dq.front().second ; dq.pop_front() ; 
-
-            for(auto [dx,dy] : dirs){
+            auto v = dq.front() ; 
+            int x = v[0] , y = v[1] , steps = v[2] ; 
+            dq.pop_front() ; 
+            for(auto& [dx,dy] : dirs){
                 int nx = x + dx , ny = y + dy ; 
-                if(nx >=0 && nx<n && ny>=0 && ny<m){
-                    if(dist[x][y] + grid[nx][ny] < dist[nx][ny]){
-                        dist[nx][ny] = dist[x][y] + grid[nx][ny] ; 
-
-                        if(grid[nx][ny] == 0){
-                            dq.push_front({nx,ny}) ; 
+                if(nx >= 0 && nx < n && ny>=0 && ny<m){
+                    if(grid[nx][ny] == 0){
+                        if(removals[nx][ny] > steps){
+                            dq.push_front({nx,ny,steps}) ; 
+                            removals[nx][ny] = steps ; 
                         }
-                        else{
-                            dq.push_back({nx,ny}) ;
+                    }
+                    else{
+                        if(removals[nx][ny] > steps+1){
+                            dq.push_back({nx,ny,steps+1}) ; 
+                            removals[nx][ny] = steps+1 ; 
                         }
                     }
                 }
             }
-
         }
 
-        return dist[n-1][m-1] ; 
+        return removals[n-1][m-1] ; 
     }
 };
