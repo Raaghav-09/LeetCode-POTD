bce126be0524e50d66d1319d7487d4717743f891
Raghav <raghavmahajanofficial09@gmail.com>
2026-07-22 01:04:12 +0530

    Time: 0 ms (100%), Space: 8.8 MB (5.02%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numTrees(int n) {
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1)) ; 
         auto f = [&](auto&& f, int low, int high)->int{
             if(low >= high) return 1 ; 
-
+            if(dp[low][high] != -1) return dp[low][high] ; 
             int ways = 0 ; 
             for(int i=low ; i<=high ; i++){
                 ways += f(f,low,i-1)*f(f,i+1,high) ; 
             }
 
-            return ways ; 
+            return dp[low][high] = ways ; 
         };
         return f(f,1,n) ; 
         // vector<int>dp(n+2,0);
