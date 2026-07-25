7a538fc513db334cb85e3fdf0826a5691c907594
Raghav <raghavmahajanofficial09@gmail.com>
2026-07-25 16:37:40 +0530

    Time: 58 ms (6.4%), Space: 32.3 MB (7.3%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,40 +3,41 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size() ; 
 
-        /*
-        need to know what will be the max profit between day i to day j
- 
-        */
-
-        vector<vector<int>> pre(n+1,vector<int>(n+1,0)) ; 
-        for(int i=0 ; i<n ; i++){
-            int buy = prices[i] ; 
-            pre[i][i] = 0 ; 
-            for(int j=i+1 ; j<n ; j++){
-                buy = min(buy,prices[j]) ; 
-                pre[i][j] = max(prices[j]-buy , pre[i][j-1] ) ; 
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(k+1,vector<int>(2,0))) ; 
+        auto f = [&](auto&& f , bool buy , int idx , int t) -> int{
+            if(t >= k) return 0 ; 
+            if(idx == n) return 0 ;
+            if(dp[idx][t][buy] != -1) return dp[idx][t][buy] ; 
+            int ans = 0 ; 
+            if(buy){
+                int res = -prices[idx] + f(f,false,idx,t) ; 
+                ans = max(res,f(f,buy,idx+1,t)) ; 
             }
-        }
+            else{
+                int res = prices[idx] + f(f,true,idx,t+1) ; ;
+                ans = max(res,f(f,buy,idx+1,t)) ; 
+            }
+            return dp[idx][t][buy] = ans ; 
+        };
 
-        // now i know what will be maximum profit between day i and j 
-        /*
-
-        dp[i][k] = maximum profit earned if I made exactly k transaction till index i
-        dp[i][k] = max(dp[i-1][k-1] + pre[i-1][i],dp[i-2][k-1] + pre[i-2][i] + .......... dp[0][k-1] + pre[i-]
         
-        */
-
-        vector<vector<int>> dp(n+1,vector<int>(k+1,0)) ; 
-        // vector<int> preMx(n,0) ; 
-        for(int i=1 ; i<n ; i++){
-            for(int t=1 ; t<=k ; t++){
-                int val = 0 ; 
-                for(int j=i ; j>=0 ; j--){
-                    val = max(val, dp[j][t-1] + pre[j][i]);
+        for(int idx=n-1 ; idx>=0 ; idx--){
+            for(int t = 1 ; t<= k ; t++){
+                for(int buy = 0 ; buy <= 1 ; buy++){
+                    int ans = 0 ; 
+                    if(buy){
+                        int res = -prices[idx] + dp[idx][t][0] ; 
+                        ans = max(res,dp[idx+1][t][buy]) ; 
+                    }
+                    else{
+                        int res = prices[idx] + dp[idx][t-1][true] ; ;
+                        ans = max(res,dp[idx+1][t][buy]) ; 
+                    }  
+                    dp[idx][t][buy] = ans ; 
                 }
-                dp[i][t] = val ; 
             }
         }
-        return dp[n-1][k] ; 
+
+        return dp[0][k][1] ; 
     }
 };
