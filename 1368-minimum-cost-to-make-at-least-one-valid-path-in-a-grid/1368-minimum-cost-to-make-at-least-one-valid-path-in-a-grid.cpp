71f2c5de64d273e5dd3f9312e751745d53fd0bd9
Raghav <raghavmahajanofficial09@gmail.com>
2026-07-31 10:29:43 +0530

    Time: 75 ms (23.41%), Space: 40.5 MB (9.78%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -3,36 +3,38 @@ public:
     int minCost(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size() ; 
 
-        vector<pair<int,int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}} ; 
+        vector<vector<int>> visited(n,vector<int>(m,INT_MAX)) ; 
 
-        deque<pair<int,int>> dq ; 
-        vector<vector<int>> dist(n,vector<int>(m,1e9)) ;
+        vector<pair<int,int>> dirs = {{0,0},{0,1},{0,-1},{1,0},{-1,0}} ; 
 
-        dq.push_front({0,0}) ; 
-        dist[0][0] = 0 ; 
+        deque<vector<int>> dq ; 
+        dq.push_front({0,0,0}) ; 
+        visited[0][0] = 1 ; 
 
         while(dq.size()){
-            int x = dq.front().first , y = dq.front().second ; 
-            dq.pop_front() ; 
+            auto v = dq.front() ; dq.pop_front() ; 
+            int cost = v[0] , x = v[1] , y = v[2] ; 
 
-            int dir = grid[x][y] - 1 ; 
-            for(int i=0 ; i<4 ; i++){
-                int nx = x + dirs[i].first , ny = y + dirs[i].second ; 
-                if(nx < 0 || nx >= n || ny < 0 || ny >=m) continue ; 
-                int cost = 1 ; 
-                if(i == dir) cost = 0 ; 
-                if(dist[x][y] + cost < dist[nx][ny]){
-                    if(cost == 0){
-                        dq.push_front({nx,ny}) ; 
+            if(x == n-1 && y == m-1) return cost ; 
+            for(int i=1 ; i<5 ; i++){
+                int nx = x + dirs[i].first , ny = y + dirs[i].second ;
+                if(nx>=0 && nx<n && ny>=0 && ny<m ){
+                    int c = (grid[x][y] != i) ; 
+                    
+                    if(visited[nx][ny] > cost+c){
+                        if(c == 1){
+                            dq.push_back({cost+c,nx,ny}) ; 
+                            visited[nx][ny] = cost + c ; 
+                        }
+                        else{
+                            dq.push_front({cost,nx,ny}) ; 
+                            visited[nx][ny] = cost ; 
+                        }
                     }
-                    else{
-                        dq.push_back({nx,ny}) ; 
-                    }
-                    dist[nx][ny] = dist[x][y] + cost ; 
                 }
             }
         }
 
-        return dist[n-1][m-1] ; 
+        return -1 ; 
     }
 };
